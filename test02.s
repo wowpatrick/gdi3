eb3e5394d3b7a4eb5ce5c40c2c17c965b8077560
clay <clay@ubuntu.(none)>
2013-11-04 17:36:19 +0100

    Fixed bug, now working

--- test02.s
+++ test02.s
@@ -8,35 +8,36 @@
     b: .long 2
 .globl main
 main:
-	movl a, %eax	    # a = 23
-	incl %eax           # next(eax) = a+1 	
+	movl a, %ebx	    # a = 23
+	incl %ebx           # next(eax) = a+1 	
 outer_loop:
 	movl b, %ecx 	    # i(ecx) = 2
 inner_loop:
-    cmpl %ecx, %eax
+    cmpl %ebx, %ecx
     jge outer_end       # while(i(ecx) < next(eax))
+    movl %ebx, %eax
 	cltd		        # convert long to double (edx:eax)
-	divl %ecx	    # eax / ebx	teiler kommt in eax, rest in edx; Operation eax / parameter 
-	cmpl $0, %ebx       # ebx === 0 (rest der division)
+	divl %ecx	        # eax / ecx	teiler kommt in eax, rest in edx; Operation eax / parameter 
+	cmpl $0, %edx       # edx === 0 (rest der division)
 	je if_inner
     jmp else_inner
 myprint:
-	pushl %eax
+	pushl %ebx
 	pushl $str
 	call printf 	    # output contents of stack according to out string
-	popl %eax	        # Clean up the stack, remove the parameters of print
+	popl %ebx	        # Clean up the stack, remove the parameters of print
 	jmp end
 end: 
 	movl $1, %eax 	    # Exit call
 	movl $0, %ebx	    # Exit call
 	int $0x80	        # Interrupt - end execturion of programm
 if_inner:
-    incl %eax
-    jmp myprint
+    incl %ebx
+    jmp outer_end 
 else_inner:
     incl %ecx
     jmp inner_loop
 outer_end:
-    cmpl %ecx, %eax
+    cmpl %ecx, %ebx
     je myprint
     jmp outer_loop
